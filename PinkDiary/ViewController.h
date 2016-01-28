919b457337be7b5e0dae1808010b3cc5971c22fc
wendyfly <wendyliao01@gmail.com>
2016-01-27 22:17:57 -0800

    Initial Commit

---
+++ PinkDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PinkDiary
+//
+//  Created by LiaoWenwen on 1/27/16.
+//  Copyright © 2016 LiaoWenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
