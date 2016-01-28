c02a7e19de559c1d78a7ff0c72a7d6f399db9a21
wendyfly <wendyliao01@gmail.com>
2016-01-27 23:36:41 -0800

    Create a basic of core data app

---
+++ PinkDiary/TheCoreDataStack.h
@@ -0,0 +1,13 @@
+//
+//  TheCoreDataStack.h
+//  PinkDiary
+//
+//  Created by LiaoWenwen on 1/27/16.
+//  Copyright © 2016 LiaoWenwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TheCoreDataStack : NSObject
+
+@end
---
+++ PinkDiary/TheCoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  TheCoreDataStack.m
+//  PinkDiary
+//
+//  Created by LiaoWenwen on 1/27/16.
+//  Copyright © 2016 LiaoWenwen. All rights reserved.
+//
+
+#import "TheCoreDataStack.h"
+
+@implementation TheCoreDataStack
+
+@end
